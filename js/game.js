9b14e2329b0007ee1de3c140a23223274ec8254b
GH0ST\joeri <99040654@mydavinci.nl>
2016-11-21 09:56:02 +0100

    Level 1 and 2 done

--- js/game.js
+++ js/game.js
@@ -1,9 +1,12 @@
 // javascript library for simple game development
 
-document.getElementById('option1').innerHTML = 'Dit is keuze 1';
-document.getElementById('level_title').innerHTML = 'Start het spel';
+document.getElementById('option1').innerHTML = 'Op vakantie gaan!';
+document.getElementById('level_title').innerHTML = 'Start je vakantie!';
+document.getElementById('uitleg').innerHTML = 'Je gaat voor je verjaardag met wat vrienden op vakantie naar de Amazone van Zuid-Amerika. Klik op de knop om op reis te gaan!'
+document.getElementById('option2').style.visibility = 'hidden';
+document.getElementById('option3').style.visibility = 'hidden';
 
-var HasKey = false;
+
 
 // jquery variant
 //$('#option1').html('Dit is keuze 1');
@@ -11,45 +14,154 @@ var HasKey = false;
 
 // https://www.youtube.com/watch?v=4ba1BqJ4S2M
 
+function start() {
+	console.log("start()");
+}
+
 
 
 function Level1() {
 	console.log("Level1()");
 
 	var opt1 = document.getElementById('option1');
-	opt1.innerHTML = 'Level 1: optie 1';
-	opt1.setAttribute("onClick", "javascript:Level2();");
+	opt1.innerHTML = 'Je besluit in de tent te blijven.';
+	opt1.setAttribute("onClick", "Level1_die();");
 
-	document.getElementById('level_title').innerHTML = 'Level 1';
-	document.getElementById('level_image').src = 'img/level1.jpg';
+	document.getElementById('level_title').innerHTML = 'In het bos';
+	document.getElementById('level_image').src = 'img/bos.jpg';
 
+	document.getElementById('uitleg').innerHTML = 'Jullie waren geland en er stond meteen een busje te wachten op het vliegveld. Jullie werden naar de rand van de amazone gebracht en kregen een GPS telefoon van de reisorganisatie. Jullie worden nog een stukje verder het bos in gereden en worden daar gedropt. Je besluit een kamp te gaan maken aangezien het al donker word en iedereen moe is.'
+	document.getElementById('uitleg2').innerHTML = '<b>BOEM</b> Midden in de nacht hoor je een grote knal, je kijkt naast je en al je vrienden zijn weg. Wat doe je?'
 
-	// only allow option 2 if user has a key in his inventory
+	// only allow option 2 if user has a phone in his inventory
 
 	var opt2 = document.getElementById('option2');
-	opt2.innerHTML = 'Level 1: optie 1';
-	opt2.onclick = function(){
-		if(HasKey){
-			Level2();
-		} else {
-			alert('U dont haz key dumbass');
-		}
-	}
+	document.getElementById('option2').style.visibility = 'visible';
+	opt2.innerHTML = 'Je gaat naar buiten';
+	opt2.setAttribute("onClick", "Level2_1();");
 
 	var opt3 = document.getElementById('option3');
-
-	opt3.onclick = function(){
-		HasKey = true;
-	}
+	document.getElementById('option3').style.visibility = 'visible';
+	opt3.innerHTML = 'Het is niks, je slaapt verder.'
+	opt3.setAttribute("onClick", "Level_sleep();");
 }
 
-function Level2() {
-	console.log("Level2()");
+function Level1_die() {
+	console.log("Level1_die()");
+
+	var opt1 = document.getElementById('option1')
+	document.getElementById('option1').style.visibility = 'visible';
+	opt1.innerHTML = 'Klik om door te gaan...'
+	opt1.setAttribute("onClick", "Leveldie();");
+
+	document.getElementById('uitleg').innerHTML = 'Je hebt besloten in de tent te blijven. Je hoort buiten geritsel dat steeds dichter bij de tent komt.'
+	document.getElementById('uitleg2').innerHTML = 'Het is nu naast de tent en opeens is er een grote flits....'
+	document.getElementById('option2').style.visibility = 'hidden';
+	document.getElementById('option3').style.visibility = 'hidden';
+
+}
+
+function Leveldie() {
+	console.log("Leveldie()");
+
+	document.getElementById('uitleg').innerHTML = 'Jou slechte keuzes hebben jou geleid tot je dood'
+	document.getElementById('uitleg2').innerHTML = ''
+	document.getElementById('level_title').innerHTML = 'Dead';
+	document.getElementById('level_image').src = 'img/gameover.jpg';
+	document.getElementById('option1').style.visibility = 'visible';
+	document.getElementById('option2').style.visibility = 'hidden';
+	document.getElementById('option3').style.visibility = 'hidden';
 
 	var opt1 = document.getElementById('option1');
-	opt1.innerHTML = 'Level 2: optie 1';
-	opt1.onClick = 'javascript:Level3();';
+	document.getElementById('option1').style.visibility = 'visible';
+	opt1.innerHTML = 'Opnieuw proberen';
+	opt1.setAttribute("onClick", "start();");
 
-	document.getElementById('level_title').innerHTML = 'Level 2';
-	document.getElementById('level_image').src = 'img/level2.jpg';
+}
+
+function Level2_1() {
+	console.log("Level2_1()");
+
+	document.getElementById('uitleg').innerHTML = 'Er staat een man buiten, je hoort hem wat woorden mompelen.'
+	document.getElementById('uitleg2').innerHTML = 'Iets over een geheim in het bos. Wat doe je?'
+	document.getElementById('level_title').innerHTML = 'In het bos';
+	document.getElementById('level_image').src = 'img/manbos.jpg';
+
+	var opt1 = document.getElementById('option1');
+	document.getElementById('option1').style.visibility = 'visible';
+	opt1.innerHTML = 'Je volgt hem stiekem';
+	opt1.setAttribute("onClick", "Level2_2();");
+
+	var opt2 = document.getElementById('option2');
+	document.getElementById('option2').style.visibility = 'visible';
+	opt2.innerHTML = 'Je valt hem aan';
+	opt2.setAttribute("onClick", "Level2_3();");
+
+	var opt3 = document.getElementById('option3');
+	document.getElementById('option3').style.visibility = 'visible';
+	opt3.innerHTML = 'Je spreekt hem aan';
+	opt3.setAttribute("onClick", "Level2_die();");
+}
+
+function Level2_die() {
+	console.log("Level2_die()");
+
+	document.getElementById('uitleg').innerHTML = 'Je spreekt hem aan en dan.'
+	document.getElementById('uitleg2').innerHTML = 'Ziet hij je, gaat schreeuwen, rent op je af en dan...'
+	document.getElementById('level_title').innerHTML = 'In het bos';
+	document.getElementById('level_image').src = 'img/manbos.jpg';
+	document.getElementById('option2').style.visibility = 'hidden';
+	document.getElementById('option3').style.visibility = 'hidden';
+
+	var opt1 = document.getElementById('option1');
+	document.getElementById('option1').style.visibility = 'visible';
+	opt1.innerHTML = 'Klik om door te gaan';
+	opt1.setAttribute("onClick", "Leveldie();");
+}
+
+function Level2_3() {
+	console.log("Level2_3()");
+
+	document.getElementById('uitleg').innerHTML = 'Je valt hem aan maar hebt geen wapen om hem goed neer te slaan. Jullie rollen over de grond en hij krijgt de overmacht.'
+	document.getElementById('uitleg2').innerHTML = 'Hij zit nu op jou je te slaan. Het word zwart voor je ogen'
+	document.getElementById('level_title').innerHTML = 'In het bos';
+	document.getElementById('level_image').src = 'img/manbos.jpg';
+	document.getElementById('option2').style.visibility = 'hidden';
+	document.getElementById('option3').style.visibility = 'hidden';
+
+	var opt1 = document.getElementById('option1');
+	document.getElementById('option1').style.visibility = 'visible';
+	opt1.innerHTML = 'Klik om door te gaan';
+	opt1.setAttribute("onClick", "Leveldie();");
+
+}
+
+function Level_sleep() {
+	console.log("Level_sleep()");
+
+	document.getElementById('uitleg').innerHTML = 'Je ligt verder te slapen, en opeens schiet er iets door je tent heen.'
+	document.getElementById('uitleg2').innerHTML = 'Je word licht in je hoofd en merkt dat je een injectie naald in hebt gekregen.'
+	document.getElementById('level_title').innerHTML = 'In tent'
+	document.getElementById('level_image').src = 'img/intent.jpg'
+	document.getElementById('option2').style.visibility = 'hidden';
+	document.getElementById('option3').style.visibility = 'hidden';
+
+	var opt1 = document.getElementById('option1');
+	opt1.innerHTML = 'Klik om door te gaan';
+	opt1.setAttribute("onClick", "Level2_4();");
+}
+
+function Level2_4() {
+	console.log("Level2_4()");
+
+	document.getElementById('uitleg').innerHTML = ''
+}
+
+function Level2_2() {
+	console.log("Levl2_2()");
+
+	document.getElementById('uitleg').innerHTML = ''
+	document.getElementById('uitleg2').innerHTML = ''
+	document.getElementById('level_title').innerHTML = ''
+	document.getElementById('level_image').src = ''
 }
